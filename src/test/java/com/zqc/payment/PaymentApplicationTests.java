85fdf221f2a4028d941aca743501fe3416abf7c2
zhangquancheng <zhang_qch@hisuntech.com>
2020-01-29 16:45:59 +0800

    first commit

---
+++ src/test/java/com/zqc/payment/PaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zqc.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
